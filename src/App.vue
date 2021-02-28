9a95940f5260cc6e15fdc8eee84f4cc62879cfef
antoine92190 <dupontantoine92@gmail.com>
2021-02-28 19:42:06 +0100

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<chat-window
+			height="calc(100vh - 20px)"
+			:current-user-id="currentUserId"
+			:rooms="rooms"
+			:rooms-loaded="true"
+			:messages="messages"
+			:messages-loaded="messagesLoaded"
+			@send-message="sendMessage"
+			@fetch-messages="fetchMessages"
+		/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import ChatWindow from 'vue-advanced-chat';
+// import 'vue-advanced-chat/dist/vue-advanced-chat.css';
+import ChatWindow from '../../vue-advanced-chat/src/ChatWindow'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'Chat',
+
+	components: {
+		ChatWindow
+	},
+
+	data() {
+		return {
+			currentUserId: 1234,
+			messages: [],
+			messagesLoaded: false,
+			rooms: []
+		}
+	},
+
+	mounted() {
+		this.rooms = [
+			{
+				roomId: 1,
+				roomName: 'Room 1',
+				avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
+				users: [
+					{ _id: 1234, username: 'John Doe' },
+					{ _id: 4321, username: 'John Snow' }
+				]
+			}
+		]
+	},
+
+	methods: {
+		fetchMessages() {
+			setTimeout(() => {
+				let messages = []
+
+				for (let i = 0; i < 30; i++) {
+					messages.push({
+						_id: i,
+						content: 'message 1',
+						senderId: 1234,
+						username: 'John Doe',
+						date: '13 November',
+						timestamp: '10:20'
+					})
+				}
+
+				this.messages = messages
+				this.messagesLoaded = true
+			}, 500)
+		},
+
+		sendMessage(message) {
+			const m = {
+				_id: this.messages.length,
+				content: message.content,
+				senderId: this.currentUserId,
+				timestamp: new Date().toString().substring(16, 21),
+				date: new Date().toDateString()
+			}
+
+			this.$set(this.messages, this.messages.length, m)
+		}
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+	font-family: 'Quicksand', sans-serif;
 }
 </style>
