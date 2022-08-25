b879476cbe36403fa47e0f3b829680da1ca18f67
antoine92190 <dupontantoine92@gmail.com>
2022-08-25 22:17:54 +0200

    update to web component version

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,35 @@
 <template>
 	<div>
-		<chat-window
+		<vue-advanced-chat
 			height="calc(100vh - 20px)"
 			:current-user-id="currentUserId"
-			:rooms="rooms"
+			:rooms="JSON.stringify(rooms)"
 			:rooms-loaded="true"
-			:messages="messages"
+			:messages="JSON.stringify(messages)"
 			:messages-loaded="messagesLoaded"
-			@send-message="sendMessage"
-			@fetch-messages="fetchMessages"
+			@send-message="sendMessage($event.detail[0])"
+			@fetch-messages="fetchMessages($event.detail[0])"
 		/>
 	</div>
 </template>
 
 <script>
-import ChatWindow from 'vue-advanced-chat'
-import 'vue-advanced-chat/dist/vue-advanced-chat.css'
-// import ChatWindow from '../../vue-advanced-chat/src/ChatWindow'
+import { register } from 'vue-advanced-chat'
+// import { register } from '../../vue-advanced-chat/dist/vue-advanced-chat.es.js'
+register()
 
 export default {
-	name: 'Chat',
-
-	components: {
-		ChatWindow
-	},
-
 	data() {
 		return {
-			currentUserId: 1234,
+			currentUserId: '1234',
 			rooms: [
 				{
-					roomId: 1,
+					roomId: '1',
 					roomName: 'Room 1',
 					avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
 					users: [
-						{ _id: 1234, username: 'John Doe' },
-						{ _id: 4321, username: 'John Snow' }
+						{ _id: '1234', username: 'John Doe' },
+						{ _id: '4321', username: 'John Snow' }
 					]
 				}
 			],
@@ -64,7 +58,7 @@ export default {
 				messages.push({
 					_id: reset ? i : this.messages.length + i,
 					content: `${reset ? '' : 'paginated'} message ${i + 1}`,
-					senderId: 4321,
+					senderId: '4321',
 					username: 'John Doe',
 					date: '13 November',
 					timestamp: '10:20'
@@ -94,7 +88,7 @@ export default {
 					{
 						_id: this.messages.length,
 						content: 'NEW MESSAGE',
-						senderId: 4321,
+						senderId: '1234',
 						timestamp: new Date().toString().substring(16, 21),
 						date: new Date().toDateString()
 					}
