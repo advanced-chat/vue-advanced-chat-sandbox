d3f2e7588ce9d2b8c56bf007f433ce61dc5f0062
antoine92190 <dupontantoine92@gmail.com>
2022-09-07 07:23:55 +0200

    update nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,107 +1,112 @@
 <template>
-	<client-only>
-		<chat-window
-			height="calc(100vh - 60px)"
-			:current-user-id="currentUserId"
-			:rooms="rooms"
-			:rooms-loaded="true"
-			:messages="messages"
-			:messages-loaded="messagesLoaded"
-			@send-message="sendMessage"
-			@fetch-messages="fetchMessages"
-		/>
-	</client-only>
+  <client-only>
+    <vue-advanced-chat
+      height="calc(100vh - 20px)"
+      :current-user-id="currentUserId"
+      :rooms="JSON.stringify(rooms)"
+      :rooms-loaded="true"
+      :messages="JSON.stringify(messages)"
+      :messages-loaded="messagesLoaded"
+      @send-message="sendMessage($event.detail[0])"
+      @fetch-messages="fetchMessages($event.detail[0])"
+    />
+  </client-only>
 </template>
 
-<script>
-import 'vue-advanced-chat/dist/vue-advanced-chat.css'
+<script lang="ts">
+import Vue from 'vue'
+import { register } from 'vue-advanced-chat/dist/vue-advanced-chat.es.js'
+register()
 
-const importChat = () => {
-	if (process.client) {
-		return { ChatWindow: () => import('vue-advanced-chat') }
-		// return {
-		// 	ChatWindow: () =>
-		// 		import('../../vue-advanced-chat/dist/vue-advanced-chat.umd.js')
-		// }
-	}
-	return {}
+interface Message {
+  _id: string
+  content: string
+  senderId: string
+  username?: string
+  timestamp: string
+  date: string
 }
 
-export default {
-	name: 'Chat',
-	components: importChat(),
-	data() {
-		return {
-			currentUserId: 1234,
-			rooms: [
-				{
-					roomId: 1,
-					roomName: 'Room 1',
-					avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
-					users: [
-						{ _id: 1234, username: 'John Doe' },
-						{ _id: 4321, username: 'John Snow' }
-					]
-				}
-			],
-			messages: [],
-			messagesLoaded: false
-		}
-	},
-	methods: {
-		fetchMessages({ options = { reset: false } }) {
-			setTimeout(() => {
-				if (options.reset) {
-					this.messages = this.addMessages(true)
-				} else {
-					this.messages = [...this.addMessages(), ...this.messages]
-					this.messagesLoaded = true
-				}
-				// this.addNewMessage()
-			})
-		},
-		addMessages(reset = false) {
-			const messages = []
-			for (let i = 0; i < 30; i++) {
-				messages.push({
-					_id: reset ? i : this.messages.length + i,
-					content: `${reset ? '' : 'paginated'} message ${i + 1}`,
-					senderId: 4321,
-					username: 'John Doe',
-					date: '13 November',
-					timestamp: '10:20'
-				})
-			}
-			return messages
-		},
-		sendMessage(message) {
-			this.messages = [
-				...this.messages,
-				{
-					_id: this.messages.length,
-					content: message.content,
-					senderId: this.currentUserId,
-					timestamp: new Date().toString().substring(16, 21),
-					date: new Date().toDateString()
-				}
-			]
-		},
-		addNewMessage() {
-			setTimeout(() => {
-				this.messages = [
-					...this.messages,
-					{
-						_id: this.messages.length,
-						content: 'NEW MESSAGE',
-						senderId: 4321,
-						timestamp: new Date().toString().substring(16, 21),
-						date: new Date().toDateString()
-					}
-				]
-			}, 2000)
-		}
-	}
-}
+export default Vue.extend({
+  name: 'Chat',
+
+  data() {
+    return {
+      currentUserId: '1234',
+      rooms: [
+        {
+          roomId: '1',
+          roomName: 'Room 1',
+          avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
+          users: [
+            { _id: '1234', username: 'John Doe' },
+            { _id: '4321', username: 'John Snow' },
+          ],
+        },
+      ],
+      messages: [] as Message[],
+      messagesLoaded: false,
+    }
+  },
+
+  methods: {
+    fetchMessages({ options = { reset: false } }) {
+      setTimeout(() => {
+        if (options.reset) {
+          this.messages = this.addMessages(true)
+        } else {
+          this.messages = [...this.addMessages(), ...this.messages]
+          this.messagesLoaded = true
+        }
+        // this.addNewMessage()
+      })
+    },
+    addMessages(reset = false): Message[] {
+      const messages: Message[] = []
+      for (let i = 0; i < 30; i++) {
+        messages.push({
+          _id: (reset ? i : this.messages.length + i).toString(),
+          content: `${reset ? '' : 'paginated'} message ${i + 1}`,
+          senderId: '4321',
+          username: 'John Doe',
+          date: '13 November',
+          timestamp: '10:20',
+        })
+      }
+      return messages
+    },
+    sendMessage(message: Message): void {
+      this.messages = [
+        ...this.messages,
+        {
+          _id: this.messages.length.toString(),
+          content: message.content,
+          senderId: this.currentUserId,
+          timestamp: new Date().toString().substring(16, 21),
+          date: new Date().toDateString(),
+        },
+      ]
+    },
+    addNewMessage(): void {
+      setTimeout(() => {
+        this.messages = [
+          ...this.messages,
+          {
+            _id: this.messages.length.toString(),
+            content: 'NEW MESSAGE',
+            senderId: '1234',
+            timestamp: new Date().toString().substring(16, 21),
+            date: new Date().toDateString(),
+          },
+        ]
+      }, 2000)
+    },
+  },
+})
 </script>
 
-<style></style>
+<style lang="scss">
+body {
+  font-family: 'Quicksand', sans-serif;
+}
+</style>
